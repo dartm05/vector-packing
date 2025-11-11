87f7a683ceca6b0677a806872f19c2abcbe0829c
rinkudeuja0713 <rinkudeuja07@gmail.com>
2025-11-11 09:51:22 -0500

    Fix syntax error in woc/__init__.py

--- src/woc/__init__.py
+++ src/woc/__init__.py
@@ -1,5 +1,4 @@
-# Create population for WOC to learn from
-ga_population = create_initial_population(vms, server_template, args.population)"""
+"""
 Wisdom of Crowds (WoC) Implementation
 
 Analyzes patterns from successful GA solutions and builds
